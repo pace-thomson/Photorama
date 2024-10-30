16a68d770122d45750a5f7b4c14bad764b771696
pace-thomson <d00482384@utahtech.edu>
2024-10-29 18:10:33 -0600

    End of Chapter 20

---
+++ Photorama/Photo.swift
@@ -0,0 +1,22 @@
+//
+//  Photo.swift
+//  Photorama
+//
+//  Created by Pace Thomson on 10/29/24.
+//
+
+import Foundation
+
+class Photo: Codable {
+    let title: String
+    let remoteURL: URL?
+    let photoID: String
+    let dateTaken: Date
+    
+    enum CodingKeys: String, CodingKey {
+        case title
+        case remoteURL = "url_z"
+        case photoID = "id"
+        case dateTaken = "datetaken"
+    }
+}
