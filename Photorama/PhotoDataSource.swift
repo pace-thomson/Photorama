40d2aa7625584f2ec0be0bed6ebabaa5cbb4695a
Jade Thomson <d00482384@utahtech.edu>
2024-11-04 13:15:38 -0700

    end of class 11/4

---
+++ Photorama/PhotoDataSource.swift
@@ -0,0 +1,28 @@
+//
+//  PhotoDataSource.swift
+//  Photorama
+//
+//  Created by Pace Thomson on 11/4/24.
+//
+
+import UIKit
+
+class PhotoDataSource: NSObject, UICollectionViewDataSource {
+
+    var photos = [Photo]()
+
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return photos.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+
+        let identifier = "PhotoCollectionViewCell"
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: identifier, for: indexPath)
+
+        return cell
+    }
+}
